0997f42902496c0953798ad1ec7a6ac9e7680c8c
k1 <keyvanramezan@gmail.com>
2020-01-31 22:21:13 +0330

    create task Project

---
+++ app/src/main/java/com/java/androidtask/MenuActivity.java
@@ -0,0 +1,38 @@
+package com.java.androidtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        TextView txtCall = findViewById(R.id.txtCall);
+        txtCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+9821818313755"));
+                startActivity(intent);
+            }
+        });
+        TextView txtProfile = findViewById(R.id.txtProfile);
+        txtProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
